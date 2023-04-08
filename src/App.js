3e2f398bdc338f248252e21b739d28f552bd96ce
Wazir <ahsankamal25@gmail.com>
2023-04-08 17:12:06 +0500

    Just started

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Spreadop from './Spreadop';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Spreadop/> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Spreadop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Spreadop = () => {
+
+   
+      const animal = ["cat", "dog", "ik", "panda", "horse", "zebra"]
+    const birds = ["pigeon", "hen", 'sparrow', 'parrot', "quoil"]
+    
+  return (
+    console.log(...animal)
+  )
+}
+
+export default Spreadop
