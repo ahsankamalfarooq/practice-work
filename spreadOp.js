dc51ca5b962de9c5d48272f02af6e0fea3f47b18
Wazir <ahsankamal25@gmail.com>
2023-04-09 15:07:10 +0500

    spread op copleted

--- spreadOp.js
+++ spreadOp.js
@@ -1,1 +1,73 @@
-console.log("hello")
+
+//clone or copy
+
+// const numbers = [1,2,3];
+// const newNumbers = [...numbers]
+// console.log(newNumbers)
+
+//Merge two arrays
+
+// const number1 = [1,2,3];
+// const number2 = [4,5,6];
+
+// const newNumbers = [...number1, ...number2]
+// console.log(newNumbers)
+
+
+//compose
+
+// const number1 = [1,2,3];
+// const number2 = [4,5,6];
+// const newNumbers = [100,...number1, ...number2, 200]
+
+// console.log(newNumbers)
+
+//we can also use it strings
+
+// const channel = 'Mr-TDM'
+
+// const bugti = [...channel]
+
+// console.log(bugti)  //result (6) ['M', 'r', '-', 'T', 'D', 'M']
+
+
+//OBJECTS
+
+// clone (shallow clone means that the property and value will clone but not the nested one)
+
+// const order = {
+//     id: 1,
+//     date: '1/1/2023',
+//     customerName: "Bugti",
+//     items: ["burger", "chips", "pizza", 'roll']
+// }
+
+// const newOrder = {
+//     ...order
+// }
+// console.log(newOrder)
+
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+
+//Merge 2 OBJD
+
+const user = {
+    name : 'Bugti',
+    city:'karachi'
+}
+
+const address = {
+    street : 'street-1'
+}
+const newUser = {
+    ...user,...address
+}
+console.log(newUser)
+
+
+
+
+
+
+
